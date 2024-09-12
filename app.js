33a593c490518188d0f8698b1f584bcdd9e32d58
shahsiya <master_work@outlook.com>
2024-09-12 23:56:55 +0300

    порядок операторов

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-// Операторы присваивания
-let age = 18+5 ;
-age += 2;// age= age+2
-age -=2 ;// age= age-2
-age *=2 ;// age= age*2
-age /=2 ;// age= age/2
+// Порядок операторов
+const isSuited = 100-10 > 90-5 ;
+console.log(isSuited);
+//100-10 (12 приоритет)>(10 приоритет) 90-5 (12 приоритет)реальная таблицы отличается на одну единицу
+// ссылка на таблицу https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Operator_precedence
+const a = (6+10)/2 ;
+console.log(a) ;
 
-
-age ++ ;//age= age+1
-age -- ;// age= age-1
-console.log(age);
-// Операторы сравнения
-const vasya = 20 ;
-console.log(age>vasya) ;
-console.log(age>=vasya) ;
-console.log(age<vasya) ;
-console.log(age<=vasya) ;
-console.log(age==vasya) ;
-
-
+let b;
+let c ;
+b=c=100+50 ;
+console.log(b) ;
+console.log(c) ;
