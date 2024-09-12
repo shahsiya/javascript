6181ca2a8a5a6b2a44c7375149e66df275ba9c79
shahsiya <master_work@outlook.com>
2024-09-12 22:49:47 +0300

    операторы присваивания или сравнения

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-// базовые арифметические операции
-const width = 10 ;
-const height = 5 ; 
-const space = width*height ;
-const newWidth = width -4 ;
-const newWidth2 = width +4 ;
-const devision = newWidth/newWidth2 ;
-const volume = 2**3 ; //2*2*2
-console.log(volume) ;
-// строки
-const sity = 'Bursa' ;
-const street = 'Ata Bul.' ;
-console.log(sity+','+ street +''+5);// конкотенация
+// Операторы присваивания
+let age = 18+5 ;
+age += 2;// age= age+2
+age -=2 ;// age= age-2
+age *=2 ;// age= age*2
+age /=2 ;// age= age/2
+
+
+age ++ ;//age= age+1
+age -- ;// age= age-1
+console.log(age);
+// Операторы сравнения
+const vasya = 20 ;
+console.log(age>vasya) ;
+console.log(age>=vasya) ;
+console.log(age<vasya) ;
+console.log(age<=vasya) ;
+console.log(age==vasya) ;
+
+
