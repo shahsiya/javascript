203ff4983f957aeacd030af03fe9d47f21215721
shahsiya <master_work@outlook.com>
2024-09-12 22:06:26 +0300

    базовые арифметические операции

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+// базовые арифметические операции
+const width = 10 ;
+const height = 5 ; 
+const space = width*height ;
+const newWidth = width -4 ;
+const newWidth2 = width +4 ;
+const devision = newWidth/newWidth2 ;
+const volume = 2**3 ; //2*2*2
+console.log(volume) ;
+// строки
+const sity = 'Bursa' ;
+const street = 'Ata Bul.' ;
+console.log(sity+','+ street +''+5);// конкотенация
