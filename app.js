a0402574e5d58738f5c78e601f2809382a1b2483
shahsiya <master_work@outlook.com>
2024-09-13 19:57:47 +0300

    Домашняя работа

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-let day = 11-2  ;
+const day = (11-2)*5  ;
 const time = 40 ;
-let isSaati =5 ;
 let maas = 80;
-day = day*isSaati ;
-console.log(day) ;
+
 maas = maas*time ;
-console.log(day>time) ;
-console.log(maas) ;
+console.log('смогу ли я работать ?'+ (day>time)) ;
+console.log('стоимость работы составит  '  + maas +'$' ) ;
 
 
