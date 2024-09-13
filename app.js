bbc36ac51df4444eadb941777ee57e7c453c9d32
shahsiya <master_work@outlook.com>
2024-09-14 02:31:08 +0300

    правильный ответ задачи

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const yatirimci = 12000 ;
-const faiz = yatirimci *0.07 ;
-if (faiz*24>13500){
-console.log('evinizi alabilirsiniz');
+const deposit = 12000;
+const rate =0.07;
+const depositLenght =24;
+const houseCost = 13500;
+const res = deposit * (1+ rate /12)**24;
+if (res> houseCost){
+	console.log(`Можем купить. Остаток ${res - houseCost}`);
 }
-const kalanFaiz = ( faiz*24-yatirimci);
-const template = `kalan bakiyeniz
-${kalanFaiz}$`;
-console.log(template);
-
+else {
+	console.log('Купить не сможем  (((')
+}
