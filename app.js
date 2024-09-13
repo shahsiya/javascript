22e5daeb19c2548378a237b605c3aecc3c9806b5
shahsiya <master_work@outlook.com>
2024-09-13 23:35:05 +0300

    ev2

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-const money = 50;
-if  (money > 50){
-	console.log('urunumuzu alabilirsiniz');
-}
-else if (money >5){
-	console.log('frkli urun alabilirsiniz');
-}
-else {
-	console.log('bakiyeniz yetersiz');
-}
-console.log('selamlar');//взаимоисключающие операторы по этому если условие везде соблюдено то выподнится первое а остальные игнорируются так как условия выполняются по очереди сверху вниз
+const yatirimci = 12000 ;
+const faiz = `${yatirimci} *0.07` ;
+console.log(faiz);
