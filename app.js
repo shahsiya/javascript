ec0a90034fcf4992707adfa13be1c50aca857e57
shahsiya <master_work@outlook.com>
2024-09-13 22:29:34 +0300

    False из других типов

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-const age = '18';
-console.log(Number ( age )+5);
-console.log(age *3);
-const userName = 'Vasya' ;
-console.log(userName +5);
-console.log(Number (userName)+5);
-console.log(typeof NaN);
-console.log(String (4) +7);
-console.log(Boolean ('') + 10);
-console.log(true +2);
-const a = 2+ '10';
-console.log(a-10);// смотреть раздел 4 урок 18 преобразование типов
-
+console.log(Boolean (0));
+console.log(Boolean(''));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(NaN));//состояние типов в которых Boolean  всегда возвращает false 
+//смотреть урок 19 раздел 4
