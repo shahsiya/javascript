ecdc29ade4db205b26736db8a17a3acd9b8132c2
shahsiya <master_work@outlook.com>
2024-09-13 17:29:10 +0300

    домащка

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-// типы данных
-let a = 5 ;
-let b =5.6 ;
-console.log(typeof a) ;
-console.log(typeof b) ;
-a= 'string' ;
-console.log(typeof a) ;
-e = 'string' ;
-console.log(typeof e) ;
-let isAdmin = a >10 ;
-console.log(typeof isAdmin) ;
-let c =undefined ;
-console.log(typeof c) ;
-c= 5 ;
-console.log(typeof c) ;
-let d = null ;
-console.log(typeof d) ;
-//обращаться к уроку 15 типы данных
+let day = 11 ;
+const time = 40 ;
+let isGunu= 5;
+let isSaati =5 ;
+const maas = 80;
+day = -2 ;
+console.log(day) ;
 
 
