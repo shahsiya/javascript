780b8932831d0f1ea307da02ff07a65750d12722
shahsiya <master_work@outlook.com>
2024-09-14 00:24:57 +0300

    ev2

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const yatirimci = 12000 ;
 const faiz = yatirimci *0.07 ;
+if (faiz*24>13500){
+console.log('evinizi alabilirsiniz');
+}
+const kalanFaiz = ( faiz*24-yatirimci);
+const template = `kalan bakiyeniz
+${kalanFaiz}$`;
+console.log(template);
 
-console.log(faiz);
