dc1836a1e53c2db7fe09de23440db01bc2995c1e
shahsiya <master_work@outlook.com>
2024-09-13 22:58:06 +0300

    if else

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-console.log(Boolean (0));
-console.log(Boolean(''));
-console.log(Boolean(undefined));
-console.log(Boolean(null));
-console.log(Boolean(NaN));//состояние типов в которых Boolean  всегда возвращает false 
-//смотреть урок 19 раздел 4
+const money = 50;
+if  (money > 50){
+	console.log('urunumuzu alabilirsiniz');
+}
+else if (money >5){
+	console.log('frkli urun alabilirsiniz');
+}
+else {
+	console.log('bakiyeniz yetersiz');
+}
+console.log('selamlar');//взаимоисключающие операторы по этому если условие везде соблюдено то выподнится первое а остальные игнорируются так как условия выполняются по очереди сверху вниз
