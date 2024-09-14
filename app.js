fcac04a7e3a27dc500c4dae4f4abaa45012e5a36
shahsiya <master_work@outlook.com>
2024-09-14 22:14:12 +0300

    swich

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
-const secretNumber  = '7';
-if (Number(secretNumber) ===7){
-console.log('угадал строго');
+const role ='ce';
+if(role ==='meneger'){
+	console.log('meneger');
 }
-if (secretNumber==7){
-	console.log('угадал не строго');
+else if (role=== 'admin'){
+	console.log('admin');
 }
-const q= Number(prompt('введите число'));
+else if (role=== 'ceo'){
+	console.log('ceo');
+}	else{
+		console.log('seni bilmiyoruz');
+	}
 
-if (q===7){
-	console.log('!');
-}//===строгое равно == не строгое равно использовать лучше строгое равно
+
+	switch (role){
+		case 'meneger':
+			console.log('meneger');
+			break;
+			case 'admin':
+				console.log('admin');
+				break;
+				case 'ceo':
+					console.log('ceo');
+					break;
+	}
+switch (role){
+	case 'meneger'://provalivaniye
+	case 'admin':
+	console.log('ne rukovoditel');
+	break;
+	case 'ceo':
+		console.log('rukovoditel');
+		break;
+		default:
+			console.log('seni bilmiyoruz');
+}
+const num = 1;
+switch(true){
+	case num > 0:
+		console.log('dogru');
+		break;
+case num < 0:
+	console.log('yanlis');
+	break;
+	default:
+		console.log('0');
+}
