a98c3406663a2198ce2cd75c0f34a2039236661a
shahsiya <master_work@outlook.com>
2024-09-15 01:07:25 +0300

    тернарные операторы

--- app.js
+++ app.js
@@ -1,47 +1,6 @@
-const role ='ce';
-if(role ==='meneger'){
-	console.log('meneger');
-}
-else if (role=== 'admin'){
-	console.log('admin');
-}
-else if (role=== 'ceo'){
-	console.log('ceo');
-}	else{
-		console.log('seni bilmiyoruz');
-	}
+const bmwX3 = 100000;
+const fordFokus = 10000;
+const buget =200000;
+let message =buget> bmwX3?'bmw ':buget> fordFokus?'ford':'velosiped';
 
-
-	switch (role){
-		case 'meneger':
-			console.log('meneger');
-			break;
-			case 'admin':
-				console.log('admin');
-				break;
-				case 'ceo':
-					console.log('ceo');
-					break;
-	}
-switch (role){
-	case 'meneger'://provalivaniye
-	case 'admin':
-	console.log('ne rukovoditel');
-	break;
-	case 'ceo':
-		console.log('rukovoditel');
-		break;
-		default:
-			console.log('seni bilmiyoruz');
-}
-const num = 1;
-switch(true){
-	case num > 0:
-		console.log('dogru');
-		break;
-case num < 0:
-	console.log('yanlis');
-	break;
-	default:
-		console.log('0');
-}
+console.log(`${message}'almak istiyorum'`);
