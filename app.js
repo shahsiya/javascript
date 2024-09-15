d91727cea615d8b6dcdf97f203b4a1b54242bade
shahsiya <master_work@outlook.com>
2024-09-15 20:04:17 +0300

    оператор нулевого слияния

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-console.log('vasya' || 'oleg');
-console.log(false || 'oleg');
-console.log('vasya' || false);
-console.log(false || false);//при или всегда выводит первый оперант true
-
-console.log('vasya' && 'oleg');
-console.log(false && 'oleg');// при или всегда будет выводить второй оперант если первый является true и всегда будет false если одно из выражений false
-console.log('vasya' && false);
-console.log(false && false);
-
-
-let a= 'marina';
-const username =a || 'petya';
-console.log(username);
-
-const isAdmin =true;
-const filename =isAdmin && 'file mp.4';
-console.log(filename);
+let username = 0 ;
+console.log(username|| 'default username');
+console.log(username ?? 'default username');//корректно определяет null и undefined
