344f7a82b62807e7b2f06b94b84a673472fb01ba
shahsiya <master_work@outlook.com>
2024-09-15 23:40:04 +0300

    domashka

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-let username = 0 ;
-console.log(username|| 'default username');
-console.log(username ?? 'default username');//корректно определяет null и undefined
+const musteriBalanse = 10;
+const musteriBonus = 120;
+const isBanned = false;
+const isExisd = true;
+const isSelling = true;
+console.log(`${ musteriBalanse>1000|| musteriBonus>100&&isExisd && isSelling &&!isBanned?'alabilirsiniz':'alamazsiniz' }`);
