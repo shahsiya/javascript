4c5d30566c0bd75eb54ee8c38aa8ad38eff9063e
shahsiya <master_work@outlook.com>
2024-09-16 20:51:46 +0300

    funkcii v funkciyah

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-function canAccessWebsite(yas){
-	if (yas <18){
-		return 'hayir';
-	}
-	return 'evet';
+const kgFiyati = 7 ;
+const kmFiyati = 5 ;
+function hesabkg (hediye) {
+	return hediye * kgFiyati ;
 }
-console.log(canAccessWebsite(17));
-const canAccessWebsite2 = yas=>yas<18?'hayir':'evet';
-console.log(canAccessWebsite2(18));
+function hesabkm (mesafe) {
+	return mesafe * kmFiyati ;
+}
+function hesap (hediye1, hediye2, mesafe) {
+	const  price1 = hesabkg(hediye1) ;
+	const fiyat2 =hesabkg (hediye2);
+	const mesafefiyati =hesabkm (mesafe);
+	return price1 + fiyat2 + mesafefiyati ;
+}
+console.log(hesap(1,2,10));
