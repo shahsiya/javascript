63de781aafdb04d103e2504de6de1ded44fba89d
shahsiya <master_work@outlook.com>
2024-09-16 15:16:30 +0300

    domash

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-function powerTwo (num){
-	//console.log(num);
-	return num*num;
-}
-console.log(powerTwo(5));
-const poft = num=>{num*num
-	console.log(num);
-	return num*num//для коротких записей
-};
-console.log(poft(6));
+const toPowerArrow =(num, power)=>num**power;
+console.log(toPowerArrow(2,3));
