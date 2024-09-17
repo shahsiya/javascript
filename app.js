641ada6e58719e4ff4213c61e304723eef49ed4b
shahsiya <master_work@outlook.com>
2024-09-17 21:18:33 +0300

    kredit na mcbook

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const kgFiyati = 7 ;
-const kmFiyati = 5 ;
-function hesabkg (hediye) {
-	return hediye * kgFiyati ;
+//let yas =25;
+//const is = false;
+//const kredimac = 1500;
+//const kredimac2 = 100;
+//const kredimacis = 500;
+ //const kredimac3 = yas>24?kredimac+kredimacis:kredimac+kredimac2;
+function inceleme1(yas,is=false){
+switch (true) {
+	case yas>24&&is:
+		return 500;
+		case yas>24:
+			return 100;
+			default:
+				return 0;}
+} 
+
+function inceleme(yas,para,mac,is=false){
+const kredi = inceleme1(yas,is);
+return mac<=para+kredi;
 }
-function hesabkm (mesafe) {
-	return mesafe * kmFiyati ;
-}
-function hesap (hediye1, hediye2, mesafe) {
-	const  price1 = hesabkg(hediye1) ;
-	const fiyat2 =hesabkg (hediye2);
-	const mesafefiyati =hesabkm (mesafe);
-	return price1 + fiyat2 + mesafefiyati ;
-}
-console.log(hesap(1,2,10));
+console.log(inceleme(25,1900,2000))  ;
